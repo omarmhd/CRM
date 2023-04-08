4548e14d5dcd440a524111288b0cf99e4219d72b
omar al haddad <42090545+omarmhd@users.noreply.github.com>
2023-04-09 02:04:38 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,9 @@ use App\Http\Controllers\EmployeeController;
 
 
 
-route::group(["prefix"=>"admin" ,"middleware"=>'auth'],function (){
+route::group(["middleware"=>'auth'],function (){
 
-    Route::get("/dashboard",[\App\Http\Controllers\DashboardController::class,'index'])->name('dashboard');
+    Route::get("/",[\App\Http\Controllers\DashboardController::class,'index'])->name('dashboard');
     Route::resource("/clients",\App\Http\Controllers\ClientController::class);
     Route::resource("/transactions",\App\Http\Controllers\TransactionController::class);
     Route::resource("/sms",\App\Http\Controllers\SmsController::class);
